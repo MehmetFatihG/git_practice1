6f34fa4db38da0120659bf6ae6822de8727b200f
fatih <goksanfatih@gmail.com>
2022-03-30 20:24:29 +0300

    work 3 is done and ready to be pushed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
 
     //doing some work here(you can type anything you want)
 
-   //Doing awesome work 2 !
+    //Doing awesome work 2 !
 
+     //Doing awesome work 3 !
+     // it is ready to be pushed
 }
